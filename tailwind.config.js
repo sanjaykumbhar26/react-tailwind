a844db4992950775020fc5f28c19ca8e9a0b2c1b
sanjaykumbhar26 <sanjay.webidea@gmail.com>
2023-02-19 18:11:38 +0530

    select componet

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ const colors = require('tailwindcss/colors');
 module.exports = {
   content: ['./src/**/*.{html,js,jsx}', './public/index.html'],
   theme: {
+    container: {
+      center: true,
+      // default breakpoints but with 40px removed
+      screens: {
+        sm: '600px',
+        md: '728px',
+        lg: '984px',
+        xl: '1240px',
+        '2xl': '1240px',
+      },
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
